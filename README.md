7265336288bf02138027cc431b3923e81ca64aee
Andy Sherwood <sherwooda@cleanwaterservices.org>
2021-09-03 10:59:07 -0700

    Setup configuration and logging

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CWS.Temp.OstaraDemo
 Demo code to pull messages from Azure Service Bus
+
+## Prerequisite
+You'll need to setup an environment variable named SHARED_SERVICEBUS_KEY with the value of the Azure Service Bus namespace's shared access key. I'll email it to you separately. Please treat this key as a password. If you want to store it differently and not use an environment variable, that's fine. Please don't commit it to any source code repositories.
+
--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,36 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace OstaraDemo {
 
     class Program {
 
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            ILogger log = Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File(GetLogFolder(), rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            string sbConnectionString = config.GetConnectionString("ServiceBus")
+                .Replace("{{SHARED_SERVICEBUS_KEY}}", Environment.GetEnvironmentVariable("SHARED_SERVICEBUS_KEY"));
+
+            log.Information("Hello World!");
+        }
+
+        static string GetLogFolder() {
+            
+            string baseFolder = (Environment.OSVersion.Platform == PlatformID.Win32NT)
+                ? Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData)
+                : Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+            return String.Join(Path.DirectorySeparatorChar, baseFolder, "OstaraDemo", "log", "OstaraDemo.log");
         }
     }
 }
